cd038c1430e6623a60a18fc25a9147cb0eed6c66
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-06-29 02:45:18 +0900

    [FEAT]: 내 빙수 목록 조회 기능 구현

--- src/main/java/depth/hackerthon/team3/domain/user/controller/UserController.java
+++ src/main/java/depth/hackerthon/team3/domain/user/controller/UserController.java
@@ -5,9 +5,7 @@ import depth.hackerthon.team3.domain.user.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +18,10 @@ public class UserController {
     @PostMapping()
     public ResponseEntity<?> registerUser(UserReq userReq) {
         return userService.registerUser(userReq);
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> getMyBoards(@RequestParam String deviceId) {
+        return userService.getMyShavedIce(deviceId);
     }
 }
--- src/main/java/depth/hackerthon/team3/domain/user/service/UserService.java
+++ src/main/java/depth/hackerthon/team3/domain/user/service/UserService.java
@@ -1,7 +1,10 @@
 package depth.hackerthon.team3.domain.user.service;
 
+import depth.hackerthon.team3.domain.board.domain.Board;
+import depth.hackerthon.team3.domain.board.domain.repository.BoardRepository;
 import depth.hackerthon.team3.domain.user.domain.User;
 import depth.hackerthon.team3.domain.user.domain.repository.UserRepository;
+import depth.hackerthon.team3.domain.user.dto.MyShavedIceRes;
 import depth.hackerthon.team3.domain.user.dto.UserReq;
 import depth.hackerthon.team3.global.payload.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,11 +12,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
 
     // 유저 정보 DB 저장
     @Transactional
@@ -30,4 +38,35 @@ public class UserService {
 
         return ResponseEntity.ok(apiResponse);
     }
+
+
+    // 내 빙수 조회
+    public ResponseEntity<?> getMyShavedIce(String deviceId) {
+        User user = userRepository.findByDeviceId(deviceId);
+        List<Board> boardList = boardRepository.findByUser(user);
+
+        List<MyShavedIceRes> myShavedIceResList = boardList.stream()
+                .map(board -> MyShavedIceRes.builder()
+                        .boardId(board.getBoardId())
+                        .image(board.getImage())
+                        .shavedIceName(board.getShavedIceName())
+                        .description(board.getDescription())
+                        .reaction1(board.getReaction1())
+                        .reaction2(board.getReaction2())
+                        .reaction3(board.getReaction3())
+                        .reaction4(board.getReaction4())
+                        .reaction5(board.getReaction5())
+                        .createdAt(board.getCreatedAt())
+                        .build())
+                .sorted(Comparator.comparing(MyShavedIceRes::getCreatedAt).reversed())
+                .toList();
+
+        ApiResponse apiResponse = ApiResponse.builder()
+                .check(true)
+                .information(myShavedIceResList)
+                .build();
+
+        return ResponseEntity.ok(apiResponse);
+    }
+    // 내 빙수 삭제
 }
